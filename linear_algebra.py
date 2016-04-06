106321250a9c809ee18bdb25ebab39ff3b3fbf61
Nick Berejan <njberejan@gmail.com>
2016-04-06 14:56:55 -0400

    first commit

---
+++ linear_algebra.py
@@ -0,0 +1,43 @@
+#NO FOR OR WHILE LOOPS
+#NO IMPORTING LIBRARIES
+    #(built-in math operators like +, -, *, / ok)
+    #math module ok
+
+# Implement these linear algebra functions:
+#
+# vector shape in shape() !
+# vector addition in vector_add() !
+# vector subtraction in vector_sub() !
+# vector sum in vector_sum()
+# dot product in dot()
+# vector multiplication by a scalar in vector_multiply()
+# mean of multiple vectors in vector_mean()
+# magnitude in magnitude()
+#RUN nosetests to run the test
+m = [3, 4]
+n = [5, 0]
+
+v = [1, 3, 0]
+w = [0, 2, 4]
+u = [1, 1, 1]
+y = [10, 20, 30]
+z = [0, 0, 0]
+
+def shape_vectors(**kwargs):
+    for x in vector:
+        return (x += 1, )
+
+def vector_add(**kwargs)):
+    for x in zip(list_a, list_b):
+        return (x + x)
+
+def vector_sub(**kwargs)):
+    vector = []
+    for x in array:
+        return (x - x, )
+
+    array = []
+    for (x, y) in array:
+        return ((x - x), (y - y))
+
+def vector_sum(**kwargs)):
